5b656144ebdf750b7ba9476b9422c62904888d6e
Zackery Fortier <zackeryfortier@Zackerys-MacBook-Air.local>
2020-06-11 09:30:07 -0500

    Adding 2 new folder structures

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component.jsx';
-import './homepage.styles.scss';
+import HomePage from './pages/homepage/homepage.component';
+import './pages/homepage/homepage.styles.scss';
 
 function App() {
   return (
