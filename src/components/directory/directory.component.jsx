56d882e29e159a0beab704d25546319c3d6d19d8
Zackery Fortier <zackeryfortier@Zackerys-Air.attlocal.net>
2020-09-12 11:35:18 -0500

    Updated homestyles, directory, and homepage components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
 
 class Directory extends React.Component {
     constructor() {
@@ -6,32 +8,32 @@ class Directory extends React.Component {
 
         this.state = {
             sections: [{
-                title: 'hats',
+                title: 'Hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1,
                 linkUrl: 'shop/hats'
                 },
             {
-                title: 'jackets',
+                title: 'Jackets',
                 imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                 id: 2,
                 linkUrl: 'shop/jackets'
             },
             {
-                title: 'sneakers',
+                title: 'Sneakers',
                 imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                 id: 3,
                 linkUrl: 'shop/sneakers'
             },
             {
-                title: 'womens',
+                title: 'Womens',
                 imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                 size: 'large',
                 id: 4,
                 linkUrl: 'shop/womens'
             },
             {
-                title: 'mens',
+                title: 'Mens',
                 imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                 size: 'large',
                 id: 5,
@@ -40,4 +42,18 @@ class Directory extends React.Component {
             ]
         }
     }
+
+    render() {
+        return(
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({title, imageUrl, id, size}) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}></MenuItem>
+                    ))
+                }
+            </div>
+        )
+    }
 }
+
+export default Directory;
