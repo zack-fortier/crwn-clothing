1a9b8db158c40cd0580833d7bf7cc7688c5b2483
Zackery Fortier <zackeryfortier@Zackerys-Air.attlocal.net>
2020-09-20 09:28:37 -0500

    Added React Router Dom and added routing, also added some comments

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import '../menu-item/menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    // Allowing background image of each menu item to be dynamically changed
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    // On click of specific menu item go to specific link that matches menu item
+    <div 
+        className={`${size} menu-item`} 
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    {/* Allowing background image of each menu item to be dynamically changed */}
     <div 
         className='background-image'
         style={{
             backgroundImage: `url(${imageUrl})`
         }}
     />
+    {/* Capitalize title of each menu item */}
     <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
@@ -17,4 +22,4 @@ const MenuItem = ({title, imageUrl, size}) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
