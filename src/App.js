426a86ec3825265cb18005f24453da9914265281
Zackery Fortier <zackeryfortier@Zackerys-Air.attlocal.net>
2020-12-04 16:02:57 -0600

    Sign in component has been made, comments need to be added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route }  from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './App.css';
 import './pages/homepage/homepage.styles.scss';
@@ -19,6 +20,8 @@ function App() {
         <Route exact path='/' component={HomePage}></Route>
         {/* Shop page */}
         <Route path='/shop' component={ShopPage}></Route>
+        {/* Sign In and Sign Up Page */}
+        <Route path='/signin' component={SignInAndSignUpPage}></Route>
       </Switch>  
     </div>
   );
