06cd6d517168875a05210cbcd4ce1f784f83f9f7
Zackery Fortier <zackeryfortier@Zackerys-Air.attlocal.net>
2021-01-28 16:21:14 -0600

    Editing Google Sign in authentication

--- src/App.js
+++ src/App.js
@@ -5,26 +5,29 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
 import './App.css';
 import './pages/homepage/homepage.styles.scss';
 
-function App() {
-  return (
-    <div>   
-      {/* Header of all pages */}
-      <Header />
-      {/* Links to different pages */}
-      <Switch>
-        {/* Home page */}
-        <Route exact path='/' component={HomePage}></Route>
-        {/* Shop page */}
-        <Route path='/shop' component={ShopPage}></Route>
-        {/* Sign In and Sign Up Page */}
-        <Route path='/signin' component={SignInAndSignUpPage}></Route>
-      </Switch>  
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>   
+        {/* Header of all pages */}
+        <Header />
+        {/* Links to different pages */}
+        <Switch>
+          {/* Home page */}
+          <Route exact path='/' component={HomePage}></Route>
+          {/* Shop page */}
+          <Route path='/shop' component={ShopPage}></Route>
+          {/* Sign In and Sign Up Page */}
+          <Route path='/signin' component={SignInAndSignUpPage}></Route>
+        </Switch>  
+      </div>
+    );
+  }
 }
 
 export default App;
