9ba56934892be2415b30245b5d39d07559cc061b
Zackery Fortier <zackeryfortier@Zackerys-Air.attlocal.net>
2021-02-11 11:20:19 -0600

    Added a way to store user data in Firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 import './pages/homepage/homepage.styles.scss';
@@ -24,9 +24,10 @@ class App extends React.Component {
   
   // Using O Auth authentication
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
+      //this.setState({ currentUser: user});
+      //console.log(user);
     })
   }
 
